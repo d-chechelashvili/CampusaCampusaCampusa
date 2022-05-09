1bec54087aeda2004c99c9a6ad5d05f9f1fc2906
d-chechelashvili <dchec18@freeuni.edu.ge>
2022-05-09 23:05:55 +0400

    Added basic routing

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import {Route, Switch} from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import SubjectPage from "./pages/SubjectPage";
 
 function App() {
     return (
-        <React.Fragment>
-            <HomePage/>
-        </React.Fragment>
-    );
+        <Switch>
+            <Route path="/subjects/:subjectName">
+                <SubjectPage/>
+            </Route>
+            <Route path="/">
+                <HomePage/>
+            </Route>
+        </Switch>
+    )
 }
 
 export default App;
