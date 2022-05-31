185ec072cc0536d3fe02e7e58ca3d18c7ba58369
Stephane Gurgenidze <stepane.gurgenidze@kiu.edu.ge>
2022-05-31 18:25:08 +0400

    Integrated mui in subject page

--- frontend/reactjs/src/pages/SubjectPage.js
+++ frontend/reactjs/src/pages/SubjectPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 import SubjectHeader from "../components/subject_page/SubjectHeader";
-import TimeNeeded from "../components/subject_page/TimeNeeded";
+import SubjectDifficulty from "../components/subject_page/SubjectDifficulty";
 
 function SubjectPage(props) {
     return (
         <React.Fragment>
             <SubjectHeader/>
-            <TimeNeeded />
+            <SubjectDifficulty />
         </React.Fragment>
     );
 }
