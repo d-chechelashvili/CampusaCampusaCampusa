ac2f19c4a49238af81b0c92538eea42130761196
d-chechelashvili <dchec18@freeuni.edu.ge>
2022-06-21 22:17:58 +0400

    Added error handling with alerts during adding subjects to planner tool

--- frontend/reactjs/src/components/subject_list/subject_adder/SubjectAdder.js
+++ frontend/reactjs/src/components/subject_list/subject_adder/SubjectAdder.js
@@ -2,16 +2,49 @@ import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 
 import {semesterActions} from "../../../store/redux-store";
-import {Box, Button, TextField, Typography} from "@mui/material";
+import {Alert, Box, Button, TextField, Typography} from "@mui/material";
 
 
 function SubjectAdder(props) {
     const dispatch = useDispatch();
     const semesterList = useSelector((state) => state.semesterList);
     const [semesterNumber, setSemesterNumber] = React.useState(1);
+    const [error, setError] = React.useState(null);
+    const [errorTimeoutId, setErrorTimeoutId] = React.useState(null);
+
 
     const handleChange = (event) => {
         setSemesterNumber(event.target.value);
+    };
+
+
+    const validSubject = () => {
+        for (let i = 0; i < semesterList.length; i++) {
+            for (const subject of semesterList[i].subjects) {
+                if (subject.name === subjectName) {
+                    if (i === semesterNumber - 1) {
+                        setError("ამ სემესტრში ეს საგანი უკვე დამატებულია")
+                        return false;
+                    } else if (subject.grade !== "F") {
+                        setError("საგანი სხვა სემესტრში უკვე ჩაბარებულია")
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    };
+
+    const addSubject = () => {
+        if (validSubject()) {
+            dispatch(semesterActions.addSubject({semesterNumber, subjectName, credits}));
+        } else {
+            clearTimeout(errorTimeoutId);
+            const id = setTimeout(() => {
+                setError(null)
+            }, 3000);
+            setErrorTimeoutId(id);
+        }
     };
 
     const subjectName = props.subjectName;
@@ -19,24 +52,21 @@ function SubjectAdder(props) {
     const semester = props.subjectSemester;
     const year = props.subjectYear;
 
-    const addSubject = (e) => {
-        dispatch(semesterActions.addSubject({semesterNumber, subjectName, credits}));
-    };
 
     const semesters = semesterList.map((semester, index) => {
         return index + 1
     });
 
     const filteredSemesters = semesters.filter((semesterNum) => {
-        const minSemester = (year-1) * 2;
+        const minSemester = (year - 1) * 2;
         if (semesterNum < minSemester) {
             return false;
         }
         switch (semester) {
             case "SPRING":
-                return semesterNum % 2 === 1;
-            case "AUTUMN":
                 return semesterNum % 2 === 0;
+            case "AUTUMN":
+                return semesterNum % 2 === 1;
             default:
                 return true;
         }
@@ -54,42 +84,50 @@ function SubjectAdder(props) {
     };
 
     return (
-        <Box sx={boxStyle} onClick={(e) => e.preventDefault()}>
-            <Box display="flex">
-                <Typography>სემესტრი: </Typography>
-                <TextField
-                    id="semester-select-native"
-                    hiddenLabel
-                    select
-                    value={semesterNumber}
-                    onChange={handleChange}
-                    sx={{marginLeft: 0.4}}
-                    SelectProps={{
-                        native: true,
-                        sx: {fontSize: "0.86rem", textAlignLast: "right"},
-                        size: "small",
-                    }}
-                    variant="standard"
+        <Box onClick={(e) => e.preventDefault()}>
+            <Box sx={boxStyle}>
+                <Box display="flex">
+                    <Typography>სემესტრი: </Typography>
+                    <TextField
+                        id="semester-select-native"
+                        hiddenLabel
+                        select
+                        value={semesterNumber}
+                        onChange={handleChange}
+                        sx={{marginLeft: 0.4}}
+                        SelectProps={{
+                            native: true,
+                            sx: {fontSize: "0.86rem", textAlignLast: "right"},
+                            size: "small",
+                        }}
+                        variant="standard"
+                        size="small"
+                    >
+                        {filteredSemesters.map((option) => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </TextField>
+                </Box>
+                <Button
                     size="small"
-                >
-                    {filteredSemesters.map((option) => (
-                        <option key={option} value={option}>
-                            {option}
-                        </option>
-                    ))}
-                </TextField>
+                    variant="contained"
+                    onClick={addSubject}
+                    sx={{
+                        fontSize: "0.85rem",
+                        background: "#0277bd",
+                    }}>
+                    დამგეგმავში დამატება
+                </Button>
             </Box>
-            <Button
-                size="small"
-                variant="contained"
-                onClick={addSubject}
-                sx={{
-                    fontSize: "0.85rem",
-                    background: "#0277bd",
-                }}>
-                დამგეგმავში დამატება
-            </Button>
+            {error &&
+                <Alert onClick={() => setError(null)}
+                       onClose={() => {}}
+                       sx={{marginTop: 0.5}} severity="error">{error}</Alert>
+            }
         </Box>
+
     );
 }
 
