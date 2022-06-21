2a697fcccf632c759104e02b656a36fec9691ca5
d-chechelashvili <dchec18@freeuni.edu.ge>
2022-06-21 19:03:20 +0400

    Added total credits and GPA to planner tool

--- frontend/reactjs/src/components/planner/Planner.js
+++ frontend/reactjs/src/components/planner/Planner.js
@@ -1,7 +1,7 @@
 import {useDispatch, useSelector} from 'react-redux';
 
-import {Box, Stack} from "@mui/material";
 import Scrollbars from "react-custom-scrollbars-2";
+import {Box, Divider, Stack, Typography} from "@mui/material";
 
 import Semester from "./Semester/Semester";
 import SemesterAdder from "./Semester/SemesterAdder";
@@ -33,29 +33,47 @@ function Planner() {
         marginLeft: 2.5,
         marginRight: {xs: 2.5, sm: 0},
         height: "calc(100% - 12px)",
+        width: "100%",
     };
 
     const scrollbarStyle = {
         height: "100%",
     }
 
-    //TODO total credits and GPA არის დასამატებელი
+    const totalCredits = semesterList.reduce((acc, semester) => acc +
+            semester.subjects.reduce((acc2, subject) => acc2 + subject.credits, 0)
+        , 0);
+
+    const GPA = (Math.random() * 4.0).toFixed(2);
 
     return (
         <Box sx={boxStyle}>
-            <Scrollbars autoHide style={scrollbarStyle}>
-                <Stack spacing={2}>
-                    {semesterList.map((semester, index) => {
-                        return (
-                            <Semester onDeleteClicked={removeSemester}
-                                      onGradeChange={changeSubjectGrade}
-                                      semesterNumber={index + 1}
-                                      subjects={semester.subjects}/>
-                        );
-                    })}
-                    <SemesterAdder onClick={addSemester}></SemesterAdder>
-                </Stack>
-            </Scrollbars>
+            <Box height="93.5%">
+                <Scrollbars autoHide style={scrollbarStyle}>
+                    <Stack spacing={2}>
+                        {semesterList.map((semester, index) => {
+                            return (
+                                <Semester onDeleteClicked={removeSemester}
+                                          onGradeChange={changeSubjectGrade}
+                                          semesterNumber={index + 1}
+                                          subjects={semester.subjects}/>
+                            );
+                        })}
+                        <SemesterAdder onClick={addSemester}></SemesterAdder>
+                    </Stack>
+                </Scrollbars>
+            </Box>
+            <Box sx={{height: "6.2%", background: "#eee", marginTop: "0.3%"}}>
+                <Divider sx={{background: "#d3d3d3"}}/>
+                <Box display="flex" alignItems="center" justifyContent="space-evenly">
+                    <Typography marginTop={0.3} variant="h6" fontSize="1.05rem">
+                        სულ კრედიტი: {totalCredits}
+                    </Typography>
+                    <Typography marginTop={0.3} variant="h6" fontSize="1.05rem">
+                        GPA: {GPA}
+                    </Typography>
+                </Box>
+            </Box>
         </Box>
     );
 }
