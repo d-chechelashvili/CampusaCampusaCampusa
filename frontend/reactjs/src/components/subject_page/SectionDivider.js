8a82d2dcf4bb16fa6240af476fa5ce18cb341163
Stephane Gurgenidze <stepane.gurgenidze@kiu.edu.ge>
2022-06-29 15:30:46 +0400

    Added dividers

---
+++ frontend/reactjs/src/components/subject_page/SectionDivider.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {Divider} from "@mui/material";
+
+function SectionDivider(props) {
+    return (
+        <React.Fragment>
+            <Divider variant="middle" style={{margin: "0px 48px"}}/>
+        </React.Fragment>
+    );
+}
+
+export default SectionDivider;
--- frontend/reactjs/src/pages/SubjectPage.js
+++ frontend/reactjs/src/pages/SubjectPage.js
@@ -4,13 +4,17 @@ import SubjectHeader from "../components/subject_page/SubjectHeader";
 import SubjectDifficulty from "../components/subject_page/SubjectDifficulty";
 import SubjectScores from "../components/subject_page/SubjectScores";
 import SubjectComments from "../components/subject_page/SubjectComments";
+import SectionDivider from "../components/subject_page/SectionDivider";
 
 function SubjectPage(props) {
     return (
         <React.Fragment>
             <SubjectHeader/>
+            <SectionDivider/>
             <SubjectDifficulty/>
+            <SectionDivider/>
             <SubjectScores/>
+            <SectionDivider/>
             <SubjectComments/>
         </React.Fragment>
     );
