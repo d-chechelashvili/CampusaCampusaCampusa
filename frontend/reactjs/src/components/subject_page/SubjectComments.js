7421f161708049a52a9b5915e8c8fafb2d94931f
Stephane Gurgenidze <stepane.gurgenidze@kiu.edu.ge>
2022-06-29 19:02:52 +0400

    Added comments adding functionality

--- frontend/reactjs/src/components/subject_page/SubjectComments.js
+++ frontend/reactjs/src/components/subject_page/SubjectComments.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "./SubjectComments.module.css";
 import Paper from "@mui/material/Paper";
 import SubjectComment from "./SubjectComment";
-import {Divider, IconButton, Pagination, TextField} from "@mui/material";
+import {IconButton, Pagination, TextField} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 
+const initialComments = [
+    {
+        text: "რამე კომენტარი"
+    },
+];
+
 function SubjectComments(props) {
+    const [comments, setComments] = useState(initialComments);
+
+    let newComment = "";
+
+    function newCommentUpdated(e) {
+        newComment = e.target.value;
+    }
+
+    function addNewComment() {
+        setComments((prevComments) => {
+            const comment = {
+                text: newComment
+            };
+            return [comment, ...prevComments];
+        });
+    }
+
     const SearchButton = () => (
-        <IconButton>
+        <IconButton onClick={addNewComment}>
             <SendIcon/>
         </IconButton>
     );
@@ -20,13 +43,14 @@ function SubjectComments(props) {
                     <TextField fullWidth style={{padding: "0px 0px 48px 0px"}}
                                id="your-comment"
                                label="გაგვიზიარეთ თქვენი გამოცდილება ანონიმურად"
+                               onInput={newCommentUpdated}
                                InputProps={{endAdornment: <SearchButton/>}}
                     />
-                    <SubjectComment/>
-                    <Divider variant="fullWidth" style={{margin: "30px 0"}}/>
-                    <SubjectComment/>
-                    <Divider variant="fullWidth" style={{margin: "30px 0"}}/>
-                    <SubjectComment/>
+                    {
+                        comments.map((comment) => (
+                            <SubjectComment text={comment.text}/>
+                        ))
+                    }
                     <div className={classes.paginationContainer}>
                         <Pagination count={10}/>
                     </div>
