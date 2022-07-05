ca1ebd5449e4ab0b215dc847de7469a745b46ad2
Stephane Gurgenidze <stepane.gurgenidze@kiu.edu.ge>
2022-07-05 23:25:10 +0400

    Implemented SubjectListAPI

--- backend/views/subject_list/subject_list.py
+++ backend/views/subject_list/subject_list.py
@@ -1,17 +1,63 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-from backend.mixins import APIErrorsMixin, PublicAPIMixin
-from backend.models.subject import Subject, SubjectSerializer
+from backend.models.rating import Rating
+from backend.models.difficulty import Difficulty
 from backend.models.subject_version import SubjectVersion
+from backend.mixins import PublicAPIMixin, APIErrorsMixin
+
+
+def calculate_rating(subject_id, ratings):
+    total = 0
+    count = 0
+
+    for rating in ratings:
+        if rating.subject.id == subject_id:
+            total += rating.rating
+            count += 1
+
+    if count == 0:
+        return -1
+
+    return total / count
+
+
+def calculate_difficulty(subject_id, difficulties):
+    total = 0
+    count = 0
+
+    for difficulty in difficulties:
+        if difficulty.subject.id == subject_id:
+            total += difficulty.difficulty
+            count += 1
+
+    if count == 0:
+        return -1
+
+    return total / count
 
 
 class SubjectListAPI(PublicAPIMixin, APIErrorsMixin, APIView):
-    # permission_classes = [IsAuthenticated]
-    #example of joins
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        subjectss = SubjectVersion.objects.select_related().first()
-        print(subjectss.subject.name)
-        subjects = Subject.objects.all()
-        serializer = SubjectSerializer(subjects, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        result = []
+
+        subject_versions = SubjectVersion.objects.select_related()
+        ratings = Rating.objects.select_related()
+        difficulties = Difficulty.objects.select_related()
+
+        for subject_version in subject_versions:
+            subject = {
+                'name': subject_version.subject.name,
+                'faculty': subject_version.faculty.abbreviation,
+                'year': subject_version.year,
+                'credits': subject_version.subject.credits,
+                'rating': calculate_rating(subject_version.subject.id, ratings),
+                'difficulty': calculate_difficulty(subject_version.subject.id, difficulties),
+                'semester': subject_version.semester
+            }
+            result.append(subject)
+
+        return JsonResponse(result, safe=False)
