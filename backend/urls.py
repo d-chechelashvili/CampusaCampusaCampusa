3f0b2ec88bc47bc33a653b9052b95a8eda37846f
Stephane Gurgenidze <stepane.gurgenidze@kiu.edu.ge>
2022-07-06 01:55:41 +0400

    Minor refactoring

--- backend/urls.py
+++ backend/urls.py
@@ -5,15 +5,23 @@ from .views.subject_list.subject_list import SubjectListAPI
 from .views.planner.planner import UserPlanAPI, AddSubjectToPlanAPI, RemoveSubjectFromPlanAPI, UpdateSubjectGradeAPI
 
 urlpatterns = [
-    # authentication
+    # **************************** Authentication *************************** #
     path('google/login/', GoogleLoginAPI.as_view(), name='login-with-google'),
 
-    # subject_list
+
+    # ***************************** Subject List **************************** #
     path('subjects/', SubjectListAPI.as_view(), name='get-subject-list'),
 
-    # planner
-    path('planner/get_plan', UserPlanAPI.as_view(), name='get-user-plan'),
-    path('planner/add_subject', AddSubjectToPlanAPI.as_view(), name='add-subject-to-plan'),
-    path('planner/remove_subject', RemoveSubjectFromPlanAPI.as_view(), name='remove-subject-from-plan'),
-    path('planner/update_grade', UpdateSubjectGradeAPI.as_view(), name='update-subject-grade'),
+
+    # ******************************* Planner ******************************* #
+    path('planner/get_plan/', UserPlanAPI.as_view(), name='get-user-plan'),
+
+    # args: subject_name, semester
+    path('planner/add_subject/', AddSubjectToPlanAPI.as_view(), name='add-subject-to-plan'),
+
+    # args: subject_name, semester
+    path('planner/remove_subject/', RemoveSubjectFromPlanAPI.as_view(), name='remove-subject-from-plan'),
+
+    # args: subject_name, semester, grade
+    path('planner/update_grade/', UpdateSubjectGradeAPI.as_view(), name='update-subject-grade'),
 ]
