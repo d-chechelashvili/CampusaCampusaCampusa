f5971299d1f054e5ed6b221b75c3bd53a28c2245
Stephane Gurgenidze <stepane.gurgenidze@kiu.edu.ge>
2022-07-12 22:23:17 +0400

    Migrated to pgsql

--- project/settings.py
+++ project/settings.py
@@ -91,10 +91,14 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
